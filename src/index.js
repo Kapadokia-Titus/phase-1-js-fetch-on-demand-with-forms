68668b80e7e61380fa697eb836d3ae7e991a9686
+ <+>
2022-08-28 06:43:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
   
+
+    //targeting the form 
+    const inputForm = document.querySelector('form'); 
+    //add event listener
+    inputForm.addEventListener('submit', (event)=>{
+        event.preventDefault(); //prevents page reload/refresh
+        
+        //target a specific child in the DOM and log its output
+        const input = document.querySelector('input#searchByID');
+        
+        //fetch the server
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response=> response.json())
+        .then(data =>{
+            const title = document.querySelector('section#movieDetails h4'); 
+            const summary = document.querySelector('section#movieDetails p'); 
+
+            title.textContent= data.title; 
+            summary.textContent = data.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
