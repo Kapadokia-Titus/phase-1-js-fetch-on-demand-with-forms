e85dd8c9429aceb64c4c8da7d617a9b5cca102c0
+ <+>
2022-08-28 19:36:36 +0300

    done

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const init = () => {
         fetch(`http://localhost:3000/movies/${input.value}`)
         .then(response=> response.json())
         .then(data =>{
+            
             const title = document.querySelector('section#movieDetails h4'); 
             const summary = document.querySelector('section#movieDetails p'); 
-
             title.textContent= data.title; 
             summary.textContent = data.summary;
         })
